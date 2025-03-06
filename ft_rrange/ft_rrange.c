a26537ba93cc980906dde8ae18cf7e24fcbd3ffc
mgavorni <matus.gavornik@gmail.com>
2025-03-06 19:03:04 +0100

    Add files via upload

---
+++ ft_rrange/ft_rrange.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int *ft_rrange(int start, int end)
+{
+    int *arr;
+    
+    if(start > end)
+    {
+        arr = (int*)malloc(sizeof(int) * ((start - end) + 1));
+    }
+    else
+    {
+        arr = (int*)malloc(sizeof(int) * ((end - start) + 1));
+    }
+    int i = 0;
+    if(!arr)
+        return(NULL);
+    while(start != end)
+    {
+        arr[i++] = end;
+        if (start > end) 
+        {
+            end += 1;
+        } 
+        else 
+        {
+            end -= 1;
+        }
+    }
+    arr[i] = end;
+    return(arr);
+}
+
+int main (void)
+{
+    int start = 0;
+    int end = -3;
+    int i = 0;
+    int *result = ft_rrange(start, end);
+    while(i < 5)
+    {
+        printf("%d\n", result[i]);
+        i++;
+    }
+}
