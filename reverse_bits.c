db123371e56c126b05878ef468f7915c87ea4e71
mgavorni <matus.gavornik@gmail.com>
2025-03-07 11:04:25 +0100

    Update reverse_bits.c

--- reverse_bits.c
+++ reverse_bits.c
@@ -1,1 +1,40 @@
- 
+ unsigned char        reverse_bits(unsigned char octet)
+{
+        unsigned char        res = 0;
+        int i = 8;
+
+        while (i > 0)
+        {
+                res = res * 2 + (octet % 2);
+                octet = octet / 2;
+                i--;
+        }
+        return (res);
+}
+
+///////////////////////
+
+unsigned char reverse_bits(unsigned char octet)
+{
+    unsigned char res = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        res = (res << 1) | (octet & 1);
+        octet >>= 1;
+    }
+    return res;
+}
+
+////////////////////////////////
+unsigned char reverse_bits(unsigned char octet)
+{
+    unsigned char res = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        res = res * 2 + (octet % 2);
+        octet = octet / 2;
+    }
+    return res;
+}
