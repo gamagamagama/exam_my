f1a301280c4cca12ada3f03dc6a8ea5bac8f08b8
mgavorni <matus.gavornik@gmail.com>
2025-03-07 10:56:10 +0100

    Update print_bits.c

--- print_bits/print_bits.c
+++ print_bits/print_bits.c
@@ -11,3 +11,18 @@ void print_bits(unsigned char octet)
         i >>= 1; 
     }
 }
+
+//////////////////////////////////
+
+#include <unistd.h>
+
+void print_bits(unsigned char octet)
+{
+    for (int i = 128; i > 0; i >>= 1)
+    {
+        if (octet & i)
+            write(1, "1", 1);
+        else
+            write(1, "0", 1);
+    }
+}
